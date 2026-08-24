430f3807d56fc8e5606dda638a38a9f6c432d810
nnbabu13 <nnbabu13@gmail.com>
2026-08-24 10:51:22 +0530

    Increase body parser limit to 20MB for base64 image uploads

--- server/index.ts
+++ server/index.ts
@@ -6,8 +6,8 @@ import { createServer } from "http";
 
 export const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: false, limit: "20mb" }));
 
 // Logging middleware
 app.use((req, res, next) => {
